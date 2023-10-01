6e664f8ba0b021304e2f7e41c2c4601f8da81cf2
Harry <harrykumawat3@gmail.com>
2023-10-01 09:59:48 -0700

    first Commit

---
+++ lib/utils/AssetsManaget.dart
@@ -0,0 +1,8 @@
+class AssetsManager {
+  static const image1 = "assets/images/1.png";
+  static const image2 = "assets/images/2.png";
+  static const image3 = "assets/images/3.png";
+  static const image4 = "assets/images/4.png";
+  static const image5 = "assets/images/5.png";
+  static const image6 = "assets/images/6.png";
+}
